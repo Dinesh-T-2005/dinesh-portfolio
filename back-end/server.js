6af3f4d464b83381176e59d674a6f67be0e08ba7
Dinesh <dinesh@tresume.us>
2026-06-12 22:36:02 +0530

    Use Gmail SSL

--- back-end/server.js
+++ back-end/server.js
@@ -12,19 +12,18 @@ const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
-});console.log(process.env.EMAIL_USER);
+}); console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
 contactEmail.verify((error) => {
   if (error) {
     console.log(error);
