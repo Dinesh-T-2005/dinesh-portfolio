216cb99cf1ddbced9079e1405ae13be4c47e7e56
Dinesh <dinesh@tresume.us>
2026-06-13 01:39:04 +0530

    Update production API URL

--- back-end/server.js
+++ back-end/server.js
@@ -17,9 +17,9 @@ console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
